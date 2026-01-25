e1e3ae66c48b06e4b2a421b1bd2fa26311c4754f
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2026-01-25 19:38:14 +0500

    Initial Commit

---
+++ CreativeHub/CreativeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativeHubApp.swift
+//  CreativeHub
+//
+//  Created by Maxim Gorbatyuk on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CreativeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
